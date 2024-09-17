1c9bca038d883d2b9bc602c89b2331dfdc358a78
1737840146 <62370846+1737840146@users.noreply.github.com>
2024-09-17 20:41:08 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'ur2008999-7b887ffbc6d9fcd10a7f97ec'
+    'ur2008999-7b887ffbc6d9fcd10a7f97ec',
       ],
 
   // 日志天数
