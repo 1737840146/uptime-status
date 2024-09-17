1215b5be2e897377f0a2d097ca9740b8bb2b6ddb
1737840146 <62370846+1737840146@users.noreply.github.com>
2024-09-17 18:21:00 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,17 +7,7 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur2008999-7b887ffbc6d9fcd10a7f97ec',
-    'm793759909-682ff576ec93b3875cbc1ea3',  //主域名
-    'm793759918-47389f24c953b6897bef3374',  //Gblog
-    'm793759959-ddbde14324a3b22deedd9bfb',  //Wblog
-    'm793759966-15e14750721deac5920da1d6',  //htmlyblog
-    'm793759995-5289f4f3fdd91bd4abed25f8',  //hexoblog
-    'm793775860-57dd114424e1b6d32ba8c70c',  //qexo
-    'm793759985-d8ba5f94bd6f612a35752e8f',  //gpt
-    'm793772574-c3734fba018429ee6fbbdb3b',  //nextjs
-    'm793759930-a488739f66efe9ba9d69e23e',  //监控
-    'm797674699-d571912e29c0c36e7794ed80',  //geetshareRSS 
-  ],
+      ],
 
   // 日志天数
   CountDays: 90,
